5e3744b9fd94e368b213ec40923d1b8c5ad0a348
GeofShoemkaer <gscottshoemaker@gmail.com>
2020-09-03 14:54:18 -0500

    Final Update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Python
-This program asks for three numbers, then gives you the options to either find the LCM or the GCF between the two.
+This program asks for three numbers, then gives you the options to either find the LCM or the GCF between the two. The program will also restart you if input the wrong type of answer (it comes with full warning from the beginning). 
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-print("Give me three numbers")
-num1 = int(input("1.) "))
-num2 = int(input("2.) "))
-num3 = int(input("3.) "))
-choice = input("GCF or LCM? ")
-if(choice.lower()=="gcf"):
-  gc = min(num1,min(num2,num3))
-  while gc>0:
-    if(num1%gc==0 and num2%gc==0 and num3%gc==0):
+print("Give me three numbers\nDon't try any funny business or you're gonna restart ;)")
+while True:
+  try:
+    num1 = int(input("1.) "))
+    num2 = int(input("2.) "))
+    num3 = int(input("3.) "))
+    choice = input("GCF or LCM? ")
+    if(choice.lower()=="gcf"):
+      gc = min(num1,min(num2,num3))
+      while gc>0 and not(num1%gc==0 and num2%gc==0 and num3%gc==0):
+        gc-=1
+      print(f"The GCF is: {gc}")
       break
-    gc-=1
-  print(f"The GCF is: {gc}.")
-elif(choice.lower()=="lcm"):
-  mSum = num1*num2*num3
-  lc = max(num1,max(num2,num3))
-  while(lc <=mSum):
-    if(lc%num1==0 and lc%num2==0 and lc%num3==0):
+    elif(choice.lower()=="lcm"):
+      lc = max(num1,max(num2,num3))
+      while lc<=num1*num2*num3 and not(lc%num1==0 and lc%num2==0 and lc%num3==0):
+        lc+=1
+      print(f"The LCM is: {lc}")
       break
-    lc+=1
-  print(f"The LCM is: {lc}.")
-else:    
-  print("That is not a choice.")        
+    else: FU = 42/0    
+  except:
+    print("That's not a valid input, try again.")
